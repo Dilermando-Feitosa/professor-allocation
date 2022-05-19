3b09c7683444eadb5f09445d61968f7a24095944
dilermando.feitosa <dilermando.feitosa@gmail.com>
2022-05-18 23:49:34 -0300

    Criação das classes de teste

--- professor-allocation-dilermando/src/test/java/com/project/professor/allocation/dilermando/service/test/AllocationServiceTest.java
+++ professor-allocation-dilermando/src/test/java/com/project/professor/allocation/dilermando/service/test/AllocationServiceTest.java
@@ -34,19 +34,25 @@ public class AllocationServiceTest {
     @Test
     public void findByProfessor() {
         // Arrange
+        Long id = 1L;
 
         // Act
+        List<Allocation> allocations = allocationService.findByProfessor(id);
 
         // Print
+        allocations.forEach(System.out::println);
     }
 
     @Test
     public void findByCourse() {
         // Arrange
+        Long id = 1L;
 
         // Act
+        List<Allocation> allocations = allocationService.findByCourse(id);
 
         // Print
+        allocations.forEach(System.out::println);
     }
 
     @Test
@@ -82,20 +88,32 @@ public class AllocationServiceTest {
     @Test
     public void update() throws ParseException {
         // Arrange
+        Allocation allocation = new Allocation();
+        allocation.setId(1L);
+        allocation.setDayOfWeek(DayOfWeek.MONDAY);
+        allocation.setStartHour(sdf.parse("19:00-0300"));
+        allocation.setEndHour(sdf.parse("20:00-0300"));
+        allocation.setProfessorId(1L);
+        allocation.setCourseId(1L);
 
         // Act
+        allocation = allocationService.update(allocation);
 
+        // Print
+        System.out.println(allocation);
     }
 
-    @Test
     public void deleteById() {
         // Arrange
+        Long id = 1L;
 
         // Act
+        allocationService.deleteById(id);
     }
 
     @Test
     public void deleteAll() {
         // Act
+        allocationService.deleteAll();
     }
 }
